3acba2212aeec7fd9e8f2481304b2820f5d4d21e
snehashirke22 <104902552+snehashirke22@users.noreply.github.com>
2022-05-08 18:55:28 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,14 @@
+x = int(input("Enter first number :"))
+y = int(input("Enter second number :"))
+print("Enter which operation would you like to perform")
+op = input("Enter operator + , - , * , / :") 
+if op =='+' :
+    print(x, op , y ,'=',x+y)
+elif op == '-' :
+    print(x, op , y , '=',x-y)
+elif op == '*' :
+    print(x, op , y , '=',x*y)
+elif op == '/' :
+    print(x, op , y , '=',x/y)
+else :
+    print("Invalid Operator")
